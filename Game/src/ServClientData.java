19459558b0990458e3ed3477fe98f263e3930a4d
GuitarThanapol <GuitarThanapol@DESKTOP-T6F4JNH.local>
2019-04-27 05:57:07 +0700

    set win new

--- Game/src/ServClientData.java
+++ Game/src/ServClientData.java
@@ -12,21 +12,24 @@ import java.text.SimpleDateFormat;
 public class ServClientData extends Thread {
 	IngameFrame server;
 	public String name = "";
-	public int indexChar=0;
-	public String IP="";
+	public int indexChar = 0;
+	public String IP = "";
 	public int checkPlayer = 0;
+
 	public ServClientData(IngameFrame server) {
-		this.server =server;
+		this.server = server;
 	}
 
-	public ServClientData(IngameFrame ingameFrame, int indexCha,String nameStr) {
-		this.indexChar=indexCha;
-		this.name=nameStr;
-		this.server =ingameFrame;
+	public ServClientData(IngameFrame ingameFrame, int indexCha, String nameStr) {
+		this.indexChar = indexCha;
+		this.name = nameStr;
+		this.server = ingameFrame;
 	}
+
 	public ServClientData() {
 		// TODO Auto-generated constructor stub
 	}
+
 	public ServClientData(String text) {
 		this.name = text;
 	}
@@ -44,22 +47,22 @@ public class ServClientData extends Thread {
 					BufferedReader bufferIn = new BufferedReader(inputStream);
 					while ((line = bufferIn.readLine()) != null) {
 						String[] spt = line.split("-");
-						System.out.println("Message : "+line);
+						System.out.println("Message : " + line);
 						if (line.equalsIgnoreCase("Ready")) {
 							try {
-								
+
 								Socket sk = new Socket(ClientFindServer.ipServ, 50111);
-								
+
 								PrintStream doStream = new PrintStream(sk.getOutputStream());
-								doStream.println(FrameGame.IP+"-"+indexChar+"-"+name);
-							
+								doStream.println(FrameGame.IP + "-" + indexChar + "-" + name);
+
 								doStream.close();
 								System.out.println("ReadySucees");
 							} catch (Exception e1) {
 								System.out.println(">>>>>> " + e1);
 							}
 							System.out.println("bug bug1");
-							
+
 						}
 						if (spt.length == 4) {
 							if (spt[0].equals("chat")) {
@@ -70,49 +73,41 @@ public class ServClientData extends Thread {
 							} else {
 								int i = Integer.parseInt(spt[3]);
 								int indexCha = Integer.parseInt(spt[1]);
-								System.out.println("bug bug2  "+i);
+								System.out.println("bug bug2  " + i);
 								if (i == 0) {
-									if(spt[0].equals(FrameGame.IP))
-									{
+									if (spt[0].equals(FrameGame.IP)) {
 										server.labelWait[i].setIcon(server.CharacterPlayer[indexCha]);
-										server.labelPlayerName[i].setText("Name : "+spt[2]);
-									}
-									else {
+										server.labelPlayerName[i].setText("Name : " + spt[2]);
+									} else {
 										server.labelWait[i].setIcon(server.Character[indexCha]);
-										server.labelPlayerName[i].setText("Name : "+spt[2]);
+										server.labelPlayerName[i].setText("Name : " + spt[2]);
 									}
 									System.out.println("image 0");
 								} else if (i == 1) {
-									if(spt[0].equals(FrameGame.IP))
-									{
+									if (spt[0].equals(FrameGame.IP)) {
 										server.labelWait[i].setIcon(server.CharacterPlayer[indexCha]);
-										server.labelPlayerName[i].setText("Name : "+spt[2]);
-									}
-									else {
+										server.labelPlayerName[i].setText("Name : " + spt[2]);
+									} else {
 										server.labelWait[i].setIcon(server.Character[indexCha]);
-										server.labelPlayerName[i].setText("Name : "+spt[2]);
+										server.labelPlayerName[i].setText("Name : " + spt[2]);
 									}
 									System.out.println("image 1");
 								} else if (i == 2) {
-									if(spt[0].equals(FrameGame.IP))
-									{
+									if (spt[0].equals(FrameGame.IP)) {
 										server.labelWait[i].setIcon(server.CharacterPlayer[indexCha]);
-										server.labelPlayerName[i].setText("Name : "+spt[2]);
-									}
-									else {
+										server.labelPlayerName[i].setText("Name : " + spt[2]);
+									} else {
 										server.labelWait[i].setIcon(server.Character[indexCha]);
-										server.labelPlayerName[i].setText("Name : "+spt[2]);
+										server.labelPlayerName[i].setText("Name : " + spt[2]);
 									}
 									System.out.println("image 2");
 								} else if (i == 3) {
-									if(spt[0].equals(FrameGame.IP))
-									{
+									if (spt[0].equals(FrameGame.IP)) {
 										server.labelWait[i].setIcon(server.CharacterPlayer[indexCha]);
-										server.labelPlayerName[i].setText("Name : "+spt[2]);
-									}
-									else {
+										server.labelPlayerName[i].setText("Name : " + spt[2]);
+									} else {
 										server.labelWait[i].setIcon(server.Character[indexCha]);
-										server.labelPlayerName[i].setText("Name : "+spt[2]);
+										server.labelPlayerName[i].setText("Name : " + spt[2]);
 									}
 									System.out.println("image 3");
 								}
@@ -120,18 +115,16 @@ public class ServClientData extends Thread {
 								server.revalidate();
 							}
 						}
-						if(spt.length==3) {
-							if(spt[0].equals(server.ip)) {
-						
+						if (spt.length == 3) {
+							if (spt[0].equals(server.ip)) {
+
 								int c = Integer.parseInt(spt[2]);
 								int ran = Integer.parseInt(spt[1]);
-								if(c==0) {
+								if (c == 0) {
 									server.labelPlayer0[checkPlayer].setIcon(server.card[ran]);
-								}
-								else if(c==1) {
+								} else if (c == 1) {
 									server.labelPlayer1[checkPlayer].setIcon(server.card[ran]);
-								}
-								else if(c==2) {
+								} else if (c == 2) {
 									server.labelPlayer2[checkPlayer].setIcon(server.card[ran]);
 								}
 //								else if(c==0) {
@@ -139,29 +132,44 @@ public class ServClientData extends Thread {
 //								}
 								checkPlayer++;
 							}
-						}
-						if(spt.length==2) {
 							System.out.println("check win");
-							set();
-							int c = Integer.parseInt(spt[1]);
-							if(spt[0].equals("win")) {
-								System.out.println("Win "+c);
-								if(c==0) {
-									server.labelWinner.setIcon(server.Winner[0]);
+
+							int c = Integer.parseInt(spt[2]);
+							int i = Integer.parseInt(spt[1]);
+							if (spt[1].equals(server.ip)) {
+								if (spt[0].equals("win")) {
+									server.labelWinner.setIcon(server.Winner[i]);
 								}
-								else if(c==1) {
-									server.labelWinner.setIcon(server.Winner[1]);
-								}
-								else if(c==2) {
-									server.labelWinner.setIcon(server.Winner[2]);
-								}
-								else if(c==3) {
+								else if(spt[0].equals("lost")) {
 									server.labelWinner.setIcon(server.Winner[5]);
 								}
-								server.panel.revalidate();
-								server.revalidate();
 							}
+							server.panel.revalidate();
+							server.revalidate();
 						}
+//						if (spt.length == 2) {
+//							System.out.println("check win");
+//							set();
+//							int c = Integer.parseInt(spt[1]);
+//							if (spt[0].equals("win")) {
+//								System.out.println("Win " + c);
+//								if (c == 0) {
+//									server.labelWinner.setIcon(server.Winner[0]);
+//								} else if (c == 1) {
+//									server.labelWinner.setIcon(server.Winner[1]);
+//								} else if (c == 2) {
+//									server.labelWinner.setIcon(server.Winner[2]);
+//								} else if (c == 3) {
+//									server.labelWinner.setIcon(server.Winner[5]);
+//								}
+//
+//							}
+//							if (spt[0].equals("lost")) {
+//								server.labelWinner.setIcon(server.Winner[5]);
+//							}
+//							server.panel.revalidate();
+//							server.revalidate();
+//						}
 
 					}
 					bufferIn.close();
@@ -174,9 +182,9 @@ public class ServClientData extends Thread {
 			// TODO: handle exception
 		}
 	}
+
 	public void set() {
 		System.out.println("xxx");
 	}
 
-	
 }
