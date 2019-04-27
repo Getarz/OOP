d0dd2d9b90b7e65bf5744495fff9833c5fdf872a
GuitarThanapol <GuitarThanapol@DESKTOP-T6F4JNH.local>
2019-04-27 11:23:21 +0700

    fgg

--- Game/src/ServClientData.java
+++ Game/src/ServClientData.java
@@ -114,6 +114,26 @@ public class ServClientData extends Thread {
 								server.panel.revalidate();
 								server.revalidate();
 							}
+							
+							//************  check win *************************************
+							System.out.println("check win");
+							int c = Integer.parseInt(spt[2]);
+							int i = Integer.parseInt(spt[3]);
+							if (spt[1].equals(server.ip)) {
+								if (spt[0].equals("win")) {
+									server.labelWinner.setIcon(server.Winner[c]);
+									server.money[i] = server.money[i]+500;
+									server.money[3] = server.money[3]-500;
+									server.labelMoney[i].setText("Money : "+server.money[i]);
+								}
+								else if(spt[0].equals("lost")) {
+									server.labelWinner.setIcon(server.Winner[5]);
+									server.money[i] = server.money[i]-500;
+									server.money[3] = server.money[3]+500;
+									server.labelMoney[i].setText("Money : "+server.money[i]);
+									AudioBotWin au = new AudioBotWin();
+								}
+							}
 						}
 						if (spt.length == 3) {
 							
