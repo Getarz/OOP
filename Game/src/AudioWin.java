75ad973c65dd8028a06fef0246758450fc871382
'DALLAS <'DALLAS@PredatorG3-571.local>
2019-05-01 23:33:10 +0700

    Create Audio Win and lost new Audio!!

---
+++ Game/src/AudioWin.java
@@ -0,0 +1,32 @@
+//import java.io.File;
+//
+//import javax.sound.sampled.AudioFormat;
+//import javax.sound.sampled.AudioInputStream;
+//import javax.sound.sampled.AudioSystem;
+//import javax.sound.sampled.Clip;
+//import javax.sound.sampled.DataLine;
+//
+//public class AudioWin {
+//	File TSover;
+//	AudioInputStream stream;
+//	AudioFormat format;
+//	DataLine.Info info;
+//	Clip clip;
+//	public AudioWin() {
+//		
+//	}
+//	void Win_100Year () {
+//		try {
+//			File initailFile = new File(System.getProperty("user.dir")	
+//					+ File.separator + "BotWin.wav");
+//			stream = AudioSystem.getAudioInputStream(initailFile);
+//			format = stream.getFormat();
+//			info = new DataLine.Info(Clip.class, format);
+//			clip = (Clip) AudioSystem.getLine(info);
+//			clip.open(stream);
+//			clip.start();
+//			clip.loop(Clip.LOOP_CONTINUOUSLY);
+//		} 
+//		catch (Exception e) {}
+//	}
+//}
